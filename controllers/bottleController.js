b5590bbeac027a5cb88c0b53e390088a0883ec0b
Francois <example@email.com>
2023-08-28 14:48:23 +0200

    update queries to MongoDb

--- controllers/bottleController.js
+++ controllers/bottleController.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
+const Bottle = require('../models/bottleModel');
 
 const bottles = JSON.parse(
   fs.readFileSync(`${__dirname}/../devData/bottles.json`)
 );
 
+const idRegex = /^[0-9a-fA-F]{24}$/;
+
 exports.checkId = (req, res, next, val) => {
   console.log(`Checking ID ${val}`);
-  if (req.params.id * 1 > bottles.length - 1) {
+  if (!idRegex.test(req.params.id)) {
     return res.status(404).json({
       status: 'failed',
       message: 'Invalid ID',
@@ -27,31 +30,72 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
-exports.getAllBottles = (req, res) => {
-  res.status(200).json(bottles);
-};
-
-exports.getBottle = (req, res) => {
-  console.log(req.params.id);
-  const id = req.params.id * 1;
-
-  const bottle = bottles.find((b) => b.id === id);
-
+exports.getAllBottles = async (req, res) => {
+  const bottlesDB = await Bottle.find();
   res.status(200).json({
     status: 'success',
-    data: bottles[id],
+    results: bottlesDB.length,
+    data: bottlesDB,
   });
 };
 
-exports.createBottle = (req, res) => {
-  const newId = bottles[bottles.length - 1].id + 1;
-  const newBottle = Object.assign({ id: newId }, req.body);
-  bottles.push(newBottle);
-  fs.writeFile(
-    `${__dirname}/../devData/bottles.json`,
-    JSON.stringify(bottles),
-    (err) => {
-      res.status(201).json({ status: 'success', data: newBottle });
+// Find in file
+// exports.getBottle = (req, res) => {
+//   console.log(req.params.id);
+//   const id = req.params.id * 1;
+
+//   const bottle = bottles.find((b) => b.id === id);
+
+//   res.status(200).json({
+//     status: 'success',
+//     data: bottles[id],
+//   });
+// };
+
+// Find in MongoDB
+exports.getBottle = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const bottle = await Bottle.findById(id);
+    if (!bottle) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'not found',
+      });
     }
-  );
+    res.status(200).json({
+      status: 'success',
+      data: bottle,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      status: 'failed',
+      message: 'Internal server error',
+    });
+  }
+};
+
+// Save to file
+// exports.createBottle = (req, res) => {
+//   const newId = bottles[bottles.length - 1].id + 1;
+//   const newBottle = Object.assign({ id: newId }, req.body);
+//   bottles.push(newBottle);
+//   fs.writeFile(
+//     `${__dirname}/../devData/bottles.json`,
+//     JSON.stringify(bottles),
+//     (err) => {
+//       res.status(201).json({ status: 'success', data: newBottle });
+//     }
+//   );
+// };
+
+// Save to MongoDB
+exports.createBottle = async (req, res) => {
+  const bottle = new Bottle(req.body);
+  await bottle.save();
+  res.status(201).json({
+    status: 'success',
+    data: bottle,
+  });
 };
